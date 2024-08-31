866c051799aac8d82a7a3b58e9ae937a44b5a614
Shubh Solanki <codewithshubh3@gmail.com>
2024-08-31 18:58:08 +0530

    Implemented the borrow book functionality.

--- src/main/java/com/shubh/Library.java
+++ src/main/java/com/shubh/Library.java
@@ -2,14 +2,18 @@ package com.shubh;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Library {
   private List<Book> books;
   private List<User> users;
+  private Map<String, String> borrowedBooks; // Here we will map isbn and userId
 
   public Library() {
     this.books = new ArrayList<>();
     this.users = new ArrayList<>();
+    this.borrowedBooks = new HashMap<>();
 
   }
 
@@ -35,6 +39,17 @@ public class Library {
           "UserID: " + user.getUserId() + ", UserName: " + user.getName());
     }
     return users;
+  }
+
+  public boolean borrowBook(String isbn, User user) {
+    for (Book book : books) {
+      if (book.getIsbn().equals(isbn) && !borrowedBooks.containsKey(isbn)) {
+        borrowedBooks.put(isbn, user.getUserId());
+        books.remove(book);
+        return true;
+      }
+    }
+    return false;
   }
 
 }
